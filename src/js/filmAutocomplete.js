3dd039b304f28256d94410e1d7ef6ebe5e217fee
Yury Krylovich <yuricher@tut.by>
2021-01-03 22:15:01 +0100

    +

--- src/js/filmAutocomplete.js
+++ src/js/filmAutocomplete.js
@@ -6,20 +6,21 @@ import 'jquery-ui/ui/widgets/autocomplete';
 const apiKey = '35c2658e0e706d145f4d4f7e995e368f';
 
 $(() => {
-  $('#filmsList').autocomplete({
+  const $list = $('#filmsList');
+  $list.autocomplete({
     minLength: 2,
     source(req, res) {
-      const val = $('#filmsList').val();
+      const val = $list.val();
 
       $.ajax({
         url: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${val}`,
         success(data) {
           const result = data.results.map(film => {
-            const date = formatReleaseDate(film.release_date, "yyyy")
+            const date = formatReleaseDate(film.release_date);
             return ({
               label: `${film.title} (${film.vote_average.toFixed(1)}) date: ${date}`,
               value: film.id,
-            })
+            });
           });
 
           res(result, () => {
@@ -35,7 +36,7 @@ $(() => {
     select(event, ui) {
       event.preventDefault();
       // place the value into the textfield called 'select_origin'...
-      $('#filmsList').val(ui.item.label);
+      $list.val(ui.item.label);
       // ... any other tasks (like setting Hidden Fields) go here...
     },
   });
