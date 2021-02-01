75e6c6edd3751994502533d2ae936237c7b9234d
Gregory Kolenko <kolengri@users.noreply.github.com>
2021-02-02 00:52:37 +0100

    Revert "Initial Films web"

--- src/js/formatRelease.js
+++
@@ -1,9 +0,0 @@
-import { format, parseISO } from 'date-fns';
-
-export const formatReleaseDate = (date) => {
-  try {
-    return format(parseISO(date), "MMMM yyyy");
-  } catch (error) {
-    return null;
-  }
-};
